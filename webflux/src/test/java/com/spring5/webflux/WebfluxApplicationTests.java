debaa028cabe028848896efbc49e759db45851e2
sajadparacha <sajadparacha@gmail.com>
2020-06-20 06:27:27 +0300

    First commit

---
+++ webflux/src/test/java/com/spring5/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
